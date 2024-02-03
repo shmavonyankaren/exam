7bdb4e2d9e35feb088cc46084aaecb21fe667aaa
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2024-02-03 17:04:54 +0400

    last commit for js tast

--- js/singlton.js
+++ js/singlton.js
@@ -1,20 +1,38 @@
 class Singleton {
-  constructor() {
+  constructor(url) {
     if (!Singleton.instance) {
-      this.data = "a";
-      Singleton.instance = this;
+      this.#fetchFromUrl(url)
+        .then((d) => d.json())
+        .then((d) => {
+          const newData = d.groupBy("id");
+          this.data = newData;
+          Singleton.instance = this;
+        });
     }
-
     return Singleton.instance;
   }
 
-  #fetchFromUrl(url) {
-    fetch(url)
-      .then((data) => data.json())
-      .then((data) => (this.data = data));
+  async #fetchFromUrl(url) {
+    return await fetch(url);
   }
 
   getData() {
     return this.data;
   }
 }
+
+const groupBy = function (groupParam) {
+  return this.reduce(function (aggr, val) {
+    const key = val[groupParam];
+    aggr[key] = aggr[key] || [];
+    aggr[key].push(val);
+    return aggr;
+  }, {});
+};
+
+Array.prototype.groupBy = groupBy;
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const data = new Singleton(url);
+
+console.log(data.getData());
